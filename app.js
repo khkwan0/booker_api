5d7910f6aa95e29ff21e08b8175f33d2efbb4811
Kenneth Kwan <khkwan0@gmail.com>
2017-07-20 22:21:11 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('express-busboy');
+
+var monk = require('monk');
+var session = require('express-session');
+var MongoStore = require('connect-mongodb-session')(session);
+var api = require('./routes/api');
+var config = require('./config.js');
+
+var app = express();
+
+var db = monk(config.mongo.host+':'+config.mongo.port+'/'+config.mongo.db);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'nunjucks');
+
+bodyParser.extend(app, {
+  upload: true,
+  mimeTypeLimit: ['image/jpeg','image/png','image/gif'],
+  json: true,
+  urlencoded: true
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', ['http://23.239.1.81:3001']);
+// res.setHeader('Access-Control-Allow-Origin', 'http://23.239.1.81:5000');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Set-Cookie, Cookie');
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+app.use(cookieParser());
+app.use(session({
+    secret: config.session.secret,
+    store: new MongoStore({
+        uri: 'mongodb://'+config.session.store.host+':'+config.session.store.port+'/'+config.session.store.db ,
+        collection: config.session.store.collection
+    }),
+    saveUninitialized: false,
+    resave: false
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
+
+
+app.use('/', api);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  console.log(err.stack);
+  res.status(err.status || 500).send();
+});
+
+module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,203 @@
+var express = require('express');
+var router = express.Router();
+var ObjectId = require('mongodb').ObjectID;
+var fs = require('fs');
+var gm = require('gm');
+
+/* GET home page. */
+router.post('/imageupload', (req, res, next)  => {
+  if (req.files.file !== 'undefined') {
+    let newFileName = 'public/assets/tmp/'+req.files.file.uuid+'_'+req.files.file.filename;
+    fs.rename(req.files.file.file, newFileName, (e) => {
+      if (e) {
+        res.status(403).send(e);
+      }
+      newFileName = '/assets/tmp/'+req.files.file.uuid+'_'+req.files.file.filename;
+      gm(__dirname+'/../public'+newFileName)
+      .resize(800,600)
+      .write(__dirname+'/../public'+newFileName+'_resize', (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(JSON.stringify({res: err}));
+        }
+      });
+      gm(__dirname+'/../public'+newFileName)
+      .resize(200,200)
+      .write(__dirname+'/../public'+newFileName+'_th', (err) => {
+        if (!err) {
+          let result = {
+            status: 'OK',
+            filename: newFileName+'_resize',
+            th: newFileName+'_th',
+            orig: req.files.file.uuid+'_'+req.files.file.filename
+          }
+          res.status(200).send(JSON.stringify(result));
+        } else {
+          console.log(err);
+          res.status(500).send(JSON.stringify({res: err}));
+        }
+      });
+    });
+  } else {
+    res.status(403).send(JSON.stringify(req));
+  }
+});
+
+router.post('/login', (req, res, next) => {
+    if (req.body.email && req.body.pw) {
+      let Users = req.db.collection('users');
+      let rv = {
+        user: null
+      };
+      Users.findOne({email:req.body.email, password: req.body.pw}, '-password')
+      .then((result) => {
+        if (result) {
+          rv.user = result;
+          req.session.user = result;
+        }
+        res.status(200).send(JSON.stringify(rv));
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(200).send(JSON.stringify({err: err.stack}));
+      });
+    } else {
+      res.status(403).send();
+    }
+});
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy();
+  res.status(200).send();
+});
+
+router.get('/checksession', (req, res, next) => {
+    let rv = {
+      user: null
+    };
+    if (typeof req.session !== 'undefined' && typeof req.session.user !== 'undefined' && req.session.user) {
+      rv.user = req.session.user;
+    }
+    res.status(200).send(JSON.stringify(rv));
+});
+
+router.get('/checkemail', (req, res, next) => {
+  let email = req.query.email;
+  if (email) {
+    let rv = {ok:1};
+    let Users = req.db.collection('users');
+    Users.findOne({email: email})
+    .then((result) => {
+      if (result) {
+        rv.ok = 0;
+      }
+      res.status(200).send(JSON.stringify(rv));
+    })
+    .catch((err) => {
+        console.log(err.stack);
+        res.status(500).send(err);
+    });
+  } else {
+    res.status(403).send();
+  }
+});
+
+router.post('/register', (req, res, next) => {
+  if (req.body.email && req.body.pw) {
+    let Users = req.db.collection('users');
+    Users.insert({email: req.body.email, password: req.body.pw, ts: new Date(), verified: 0})
+    .then((result) => {
+      if (result._id) {
+        let rv = {
+          ok: 1,
+          user: {
+            email: req.body.email,
+          }
+        }
+        req.session.user = {
+          email: req.body.email
+        }
+        res.status(200).send(JSON.stringify(rv));
+      } else {
+        let rv = {
+          ok: 0,
+          msg: '0 saved. maybe duplicate?'
+        }
+        res.status(403).send(JSON.stringify(rv));
+      }
+    })
+    .catch((err) => {
+      console.log(err.stack);
+      let rv = {
+        ok: 0,
+        msg: err.stack
+      }
+      res.status(403).send(JSON.stringify(rv));
+    });
+  } else {
+    let rv = {
+      'ok': 0
+    }
+    res.status(200).send(JSON.stringify(rv));
+  }
+});
+
+router.post('/savevenue', (req, res, next) => {
+  let toSave = req.body;
+  let rv = {
+    ok: 0
+  };
+  if (toSave && req.session.user) {
+    if (toSave.orig) {
+      let dst_dir = __dirname + '/../public/assets/images/venue/';
+      let nameParts = toSave.orig.split('.');
+      let baseName = '';
+      let ext = '';
+      if (nameParts.length === 1) {
+        baseName = toSave.orig;
+      } else {
+        for (i=0; i<nameParts.length-1; i++) {
+          baseName += nameParts[i];
+        }
+        ext = '.' + nameParts[nameParts.length-1];
+      }
+      if (baseName) {
+        fs.rename(__dirname + '/../public' + toSave.image, dst_dir + baseName + '_resize' + ext, (e) => {
+          if (e) {
+            console.log(e);
+          }
+        });
+        fs.rename(__dirname + '/../public' + toSave.th, dst_dir + baseName + '_th' + ext, (e) => { 
+          if (e) {
+            console.log(e);
+          }
+        });
+        fs.rename(__dirname + '/../public/assets/tmp/' + toSave.orig, dst_dir + baseName + ext, (e) => { 
+          if (e) {
+            console.log(e);
+          }
+        });
+        toSave.image = baseName + '_resize' + ext;
+        toSave.th = baseName + '_th' + ext;
+        toSave.image = baseName + ext;
+      }
+    }
+    Venues = req.db.collection('venues');
+    toSave.ts = new Date();
+    Venues.insert(toSave)
+    .then((result) => {
+      if (result._id) {
+        rv.ok = 1;
+      }
+      res.status(200).send(JSON.stringify(rv));
+    })
+    .catch((err) => {
+      console.log(err.stack)
+      res.status(200).send(JSON.stringify(rv));
+    });
+  } else {
+    res.status(403).send(JSON.stringify(rv));
+  }
+});
+
+module.exports = router;
